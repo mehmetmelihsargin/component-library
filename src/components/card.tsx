472e15b6aaf6dba168af4fea8d94899ac8386df7
mehmetmelihsargin <mehmetmelihsargin@gmail.com>
2024-01-11 13:50:35 +0300

    first commit

---
+++ src/components/card.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Star from '../../public/assets/star';
+import Orange from '../../public/assets/orange';
+import Sales from '../../public/assets/sales';
+import View from '../../public/assets/view';
+
+const Card = () => {
+    return (
+        <div className='flex sm:flex-row flex-col p-6 gap-6'>
+            <img src='../assets/img.png' className='w-full object-cover sm:max-w-[278px]' alt="" />
+            <div className='flex flex-col'>
+                <div className='h-8 font-medium text-2xl leading-8 tracking-widest text-[#252B42]'>Floating Phone</div>
+                <div className='flex mt-4 h-6'>
+                    <p className='text-sm font-semibold leading-6 tracking-wide text-[#737373]'>By</p>
+                    <p className='text-sm font-semibold leading-6 tracking-wider ml-1 text-[#23A6F0]'>Jane Cooper</p>
+                    <p className='text-sm font-semibold leading-6 tracking-wide ml-1 text-[#737373]'>in Tech , Products</p>
+                </div>
+                <div className='flex  mt-4'>
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Orange />
+                    <p className='ml-[10px] text-sm font-semibold leading-6 tracking-wide text-[#737373]'>10 Reviews</p>
+                </div>
+                <div className='flex items-center w-[71px] h-[34px] gap-[10px] sm:hidden mt-4 rounded-md border border-solid border-gray-300 '>
+                    <p className='text-lg font-bold leading-6 tracking-wide'>$13.33</p>
+                </div>
+                <div className='flex mt-4 items-center'>
+                    <Sales />
+                    <p className='text-sm font-semibold leading-6 tracking-wide ml-[10px] text-[#737373]'>15 Sales</p>
+                </div>
+                <div className='flex items-center mt-4 sm:hidden'>
+                    <div className='border border-solid border-gray-300 w-9 items-center'>
+                        <p className='ml-2 text-xs font-bold leading-7 text-[#737373]'>XL</p>
+                    </div>
+                    <div className='border border-solid border-gray-300 w-9 items-center ml-2'>
+                        <p className='ml-2 text-xs font-bold leading-7 text-[#737373]'>L</p>
+                    </div>
+                    <div className='border border-solid border-gray-300 w-9 items-center ml-2'>
+                        <p className='ml-2 text-xs font-bold leading-7 text-[#737373]'>M</p>
+                    </div>
+                    <div className='border border-solid border-gray-300 w-9 items-center ml-2'>
+                        <p className='ml-2 text-xs font-bold leading-7 text-[#737373]'>S</p>
+                    </div>
+                </div>
+            </div>
+            <div className='flex flex-col sm:gap-[68px] sm:justify-between'>
+                <div className='hidden sm:flex flex-col items-end'>
+                    <div className='text-lg font-bold leading-6 tracking-wide rounded-md flex items-center justify-center h-11 w-[81px] text-white bg-[#40BB15]'>$13.33</div>
+                    <p className='text-sm font-semibold leading-6 tracking-wide text-[#737373]'>Starting cost</p>
+                </div>
+                <div className='flex justify-center rounded-md bg-[#23A6F0]'>
+                    <p className='text-base font-semibold leading-6 text-white sm:hidden'>Select option</p>
+                    <p className='text-base font-semibold leading-6 text-white hidden sm:block'>View profile</p>
+                </div>
+                <div className='flex items-center justify-center mt-6 w-full sm:hidden'>
+                    <View />
+                    <p className='text-sm font-semibold leading-6 tracking-wide text-[#737373] ml-2 h-6'>Quick View</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/dropdown.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+const Dropdown = () => {
+    return (
+        <div className='flex flex-col gap-10 p-10'>
+            <header className='tracking-widest text-2xl font-bold leading-8 text-center'>Book Appointment</header>
+            <div className='gap-[10px]'>
+                <div>
+                    <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3'>Name *</p>
+                    <div className='bg-[#F9F9F9] border border-solid rounded-md border-[#E6E6E6] h-[50px] flex items-center '>
+                        <input type=""
+                            className='ml-[20px] bg-[#F9F9F9] text-[#737373]'
+                            placeholder="Full Name *"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3 mt-3'>Email address *</p>
+                    <div className='bg-[#F9F9F9] border border-solid rounded-md border-[#E6E6E6] h-[50px] flex items-center '>
+                        <input type=""
+                            placeholder="example@gmail.com"
+                            className='ml-[20px] bg-[#F9F9F9] text-[#737373]'
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* <div className="App">
+                <Select
+                    defaultValue={selectedOption}
+                    onChange={setSelectedOption}
+                    options={options}
+                />
+            </div> */}
+            <div className='flex justify-center rounded-md bg-[#23A6F0] text-white p-4 tracking-tighter leading-7 font-bold text-base'>Book Appointment</div>
+        </div>
+    )
+}
+
+export default Dropdown
