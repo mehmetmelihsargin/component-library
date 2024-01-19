449618f5b82d504d9e5370871a6f8d740ec088fd
mehmetmelihsargin <mehmetmelihsargin@gmail.com>
2024-01-19 17:39:50 +0300

    form events added

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,22 +1,37 @@
 import React, { useState } from 'react'
 import Select from 'react-select';
-// import Placeholder from 'react-select/dist/declarations/src/components/Placeholder';
-// const [selectedOption, setSelectedOption] = useState<any>();
-const options = [
-    { value: '4:00 Available', label: '4:00 Available' },
-    { value: 'Please Select', label: 'Please Select' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
-  const [inpValue, setinpValue] = useState({});
-  
-  const handleSelectChange = ({}) => {
-    setinpValue({
-      ...inpValue,
-      selectedOption,
-    });
-console.log(inpValue)
-}
+
+
 const Dropdown = () => {
+    const [name, setName] = useState('');
+    const [mail, setMail] = useState('');
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [selectedTimeOption, setSelectedTimeOption] = useState(null);
+    const departments: any = [
+        { value: 'IT', label: 'IT' },
+        { value: 'HR', label: 'HR' },
+    ];
+    const times: any = [
+        { value: '14:00', label: '14:99' },
+        { value: '15:00', label: '15:00' },
+    ];
+
+    const handleChange = (e: any, inputName: string) => {
+        if (inputName === 'name') {
+            setName(e.target.value)
+        } else if (inputName === 'mail') {
+            setMail(e.target.value)
+        }
+
+    }
+    const handleSubmit = () => {
+        console.log({
+            name: name,
+            mail: mail,
+            selectedOption: selectedOption,
+            selectedTimeOption: selectedTimeOption
+        })
+    }
     return (
         <div className='flex flex-col gap-10 p-10'>
             <header className='tracking-widest text-2xl font-bold leading-8 text-center'>Book Appointment</header>
@@ -25,9 +40,11 @@ const Dropdown = () => {
                     <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3'>Name *</p>
                     <div className='bg-[#F9F9F9] border border-solid rounded-md border-[#E6E6E6] h-[50px] flex items-center '>
                         <input type=""
+                            name='name'
                             className='ml-[20px] bg-[#F9F9F9] text-[#737373]'
                             placeholder="Full Name *"
-                            onChange={handleSelectChange}
+                            onChange={(e) => handleChange(e, 'name')}
+                            value={name}
                         />
                     </div>
                 </div>
@@ -35,70 +52,73 @@ const Dropdown = () => {
                     <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3 '>Email address *</p>
                     <div className='bg-[#F9F9F9] border border-solid rounded-md border-[#E6E6E6] h-[50px] flex items-center '>
                         <input type=""
+                            name='mail'
                             placeholder="example@gmail.com"
                             className='ml-[20px] bg-[#F9F9F9] text-[#737373]'
+                            onChange={(e) => handleChange(e, 'mail')}
+                            value={mail}
                         />
                     </div>
                 </div>
                 <div className=''>
-            <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3'>Departement *</p>
-            <Select
-                    defaultValue={selectedOption}
-                    onChange={handleSelectChange}
-                    options={options}
-                    placeholder='Please Select'
-                    styles={{
-                        control: (baseStyles) => ({
-                          ...baseStyles,
-                          height:'50px',
-                          backgroundColor:'#F9F9F9',
-                          borderRadius:'5px',
-                          border:'#DDDDDD,1px,solid',
+                    <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3'>Departement *</p>
+                    <Select
+                        defaultValue={selectedOption}
+                        onChange={setSelectedOption}
+                        options={departments}
+                        placeholder='Please Select'
+                        styles={{
+                            control: (baseStyles) => ({
+                                ...baseStyles,
+                                height: '50px',
+                                backgroundColor: '#F9F9F9',
+                                borderRadius: '5px',
+                                border: '#DDDDDD,1px,solid',
 
-                        }),
-                        indicatorSeparator: (baseStyles) => ({
-                            ...baseStyles,
-                            display:'none'
-                        }),
-                        placeholder: (baseStyles) => ({
-                            ...baseStyles,
-                            marginLeft:'14px',
-                            fontSize:'14px',
-                            color:'#737373'
-                        })
-                      }}
-                />
+                            }),
+                            indicatorSeparator: (baseStyles) => ({
+                                ...baseStyles,
+                                display: 'none'
+                            }),
+                            placeholder: (baseStyles) => ({
+                                ...baseStyles,
+                                marginLeft: '14px',
+                                fontSize: '14px',
+                                color: '#737373'
+                            })
+                        }}
+                    />
                 </div>
                 <div>
-               <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3'>Time *</p>
-                <Select
-                    // defaultValue={selectedOption}
-                    // onChange={}
-                    options={options}
-                    placeholder='Please Select'
-                    styles={{
-                        control: (baseStyles) => ({
-                          ...baseStyles,
-                          height:'50px',
-                          backgroundColor:'#F9F9F9',
-                          borderRadius:'5px',
-                          border:'#DDDDDD,1px,solid',
-                        }),
-                        indicatorSeparator: (baseStyles) => ({
-                            ...baseStyles,
-                            display:'none'
-                        }),
-                        placeholder: (baseStyles) => ({
-                            ...baseStyles,
-                            marginLeft:'14px',
-                            fontSize:'14px',
-                            color:'#737373'
-                        })
-                      }}
-                />
+                    <p className='text-base font-semibold leading-6 tracking-wide text-left mb-3'>Time *</p>
+                    <Select
+                        defaultValue={selectedTimeOption}
+                        onChange={setSelectedTimeOption}
+                        options={times}
+                        placeholder='Please Select'
+                        styles={{
+                            control: (baseStyles) => ({
+                                ...baseStyles,
+                                height: '50px',
+                                backgroundColor: '#F9F9F9',
+                                borderRadius: '5px',
+                                border: '#DDDDDD,1px,solid',
+                            }),
+                            indicatorSeparator: (baseStyles) => ({
+                                ...baseStyles,
+                                display: 'none'
+                            }),
+                            placeholder: (baseStyles) => ({
+                                ...baseStyles,
+                                marginLeft: '14px',
+                                fontSize: '14px',
+                                color: '#737373'
+                            })
+                        }}
+                    />
                 </div>
             </div>
-            <div className='flex justify-center rounded-md bg-[#23A6F0] text-white p-4 tracking-tighter leading-7 font-bold text-base'>Book Appointment</div>
+            <div onClick={() => handleSubmit()} className='flex justify-center rounded-md bg-[#23A6F0] text-white p-4 tracking-tighter leading-7 font-bold text-base'>Book Appointment</div>
         </div>
     )
 }
